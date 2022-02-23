81816bd47b718a70501faaf6feb1209fe3a69648
chibataku0815 <chiba@fores-tone.co.jp>
2022-02-24 00:51:15 +0900

    Background-image: liner-gradient() was not displayed properly in safari.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,14 @@
 import { Flex, Heading } from '@chakra-ui/react'
 
 export const Hero = ({ title }: { title: string }) => (
-  <Flex
-    justifyContent="center"
-    alignItems="center"
-    height="100vh"
-    bgGradient="linear(to-l, #7928CA, #FF0080)"
-    bgClip="text"
-  >
-    <Heading fontSize="6vw">{title}</Heading>
+  <Flex justifyContent="center" alignItems="center" height="100vh">
+    <Heading
+      fontSize="6vw"
+      bgGradient="linear(to-l, #7928CA, #FF0080)"
+      bgClip="text"
+    >
+      {title}
+    </Heading>
   </Flex>
 )
 
